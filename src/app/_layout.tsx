91db05dc6a734eda0e68ccdbb6ff06ad5cc45f87
ErnandoM <ernandomanoel.freire@gmail.com>
2024-11-13 11:28:52 -0300

    feat: added feature expo prebuild

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,17 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 
 
 
 export default function RootLayout() {
 
   return (    
+    <>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />        
-        <StatusBar style="auto" />    
+        <Stack.Screen name="index"  options={{ headerShown: true }} />                
       </Stack>
+    <StatusBar style="auto" />    
+    </>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "../constants/colors";
 
 export default function Index() {
     return (
-        <View>
-            <Text>TODO SUPERMARKET</Text>
+        <View style={style.container}>
+            <Text style={style.text}>TODO SUPERMARKET</Text>
         </View>
     );
 }
+
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",        
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 20,
+
+    }
+})
